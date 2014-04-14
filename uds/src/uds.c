46c9a7a7270e083ad11c557b9b4848713c891ec1
hwchiu <sppsorrg@gmail.com>
2014-04-14 19:01:20 +0800

    1.Use a linked list to stroe all switch dpid 2.complete send function. 3.oxm_match_from_json need modify to support more filds

--- uds/src/uds.c
+++ uds/src/uds.c
@@ -24,14 +24,95 @@
 #include "simple_restapi_manager.h"
 #include "json.h"
 
-/****************************************
- _     _____    _    ____  _   _   ______        _____ _____ ____ _   _
-| |   | ____|  / \  |  _ \| \ | | / ___\ \      / /_ _|_   _/ ___| | | |
-| |   |  _|   / _ \ | |_) |  \| | \___ \\ \ /\ / / | |  | || |   | |_| |
-| |___| |___ / ___ \|  _ <| |\  |  ___) |\ V  V /  | |  | || |___|  _  |
-|_____|_____/_/   \_\_| \_\_| \_| |____/  \_/\_/  |___| |_| \____|_| |_|
 
-*///////////////////////////////////////
+
+list_element *switches;
+int send_uds_flow(uint64_t datapath_id, char* request_data,int len);
+int set_oxm_matches_from_json(oxm_matches* match,char* request_data);
+
+
+
+/******************************************************************
+ *  _     ___ _   _ _  __  _     ___ ____ _____
+ * | |   |_ _| \ | | |/ / | |   |_ _/ ___|_   _|
+ * | |    | ||  \| | ' /  | |    | |\___ \ | |
+ * | |___ | || |\  | . \  | |___ | | ___) || |
+ * |_____|___|_| \_|_|\_\ |_____|___|____/ |_|
+ *
+ *****************************************************************/
+
+
+static void
+create_switches( list_element **switches ) {
+  create_list( switches );
+}
+
+
+static void
+delete_switches( list_element *switches ) {
+  list_element *element;
+  for ( element = switches; element != NULL; element = element->next ) {
+    if ( element->data != NULL ) {
+      xfree( element->data );
+    }
+  }
+  delete_list( switches );
+}
+
+
+static void 
+insert_datapath_id( list_element **switches, uint64_t datapath_id ) {
+  list_element *element = NULL;
+  for ( element = *switches; element != NULL; element = element->next ) {
+    if ( *( ( uint64_t * ) element->data ) > datapath_id ) {
+      break;
+    }
+    if ( *( ( uint64_t * ) element->data ) == datapath_id ) {
+      // already exists
+      return;
+    }
+  }
+  uint64_t *new = xmalloc( sizeof( uint64_t ) );
+  *new = datapath_id;
+  if ( element == NULL ) {
+    append_to_tail( switches, new );
+  }
+  else if ( element == *switches ) {
+    insert_in_front( switches, new );
+  }
+  else {
+    insert_before( switches, element->data, new );
+  }
+}
+
+
+static void
+delete_datapath_id( list_element **switches, uint64_t datapath_id ) {
+  list_element *element = NULL;
+  for ( element = *switches; element != NULL; element = element->next ) {
+    if ( *( ( uint64_t * ) element->data ) == datapath_id ) {
+      void *data = element->data;
+      delete_element( switches, data );
+      xfree( data );
+      return;
+    }
+  }
+  // not found
+}
+
+
+
+
+
+
+/***************************************************
+ *  _     _____    _    ____  _   _   ______        _____ _____ ____ _   _
+ * | |   | ____|  / \  |  _ \| \ | | / ___\ \      / /_ _|_   _/ ___| | | |
+ * | |   |  _|   / _ \ | |_) |  \| | \___ \\ \ /\ / / | |  | || |   | |_| |
+ * | |___| |___ / ___ \|  _ <| |\  |  ___) |\ V  V /  | |  | || |___|  _  |
+ * |_____|_____/_/   \_\_| \_\_| \_| |____/  \_/\_/  |___| |_| \____|_| |_|
+ *
+ *******************************************/
 
 
 typedef struct {
@@ -146,7 +227,7 @@ send_packet( uint32_t destination_port, packet_in packet_in, uint32_t in_port ) 
     get_transaction_id(),
     get_cookie(),
     0,
-    0,
+    1,  //table id
     OFPFC_ADD,
     60,
     0,
@@ -244,6 +325,10 @@ static void
 handle_switch_ready( uint64_t datapath_id, void *user_data ) {
   UNUSED( user_data );
   info( "%#" PRIx64 " is connected.", datapath_id );
+  //add datapath
+  list_element **switches = user_data;
+  insert_datapath_id( switches, datapath_id );
+  
   openflow_actions *actions = create_actions();
   append_action_output( actions, OFPP_CONTROLLER, OFPCML_NO_BUFFER );
 
@@ -254,7 +339,7 @@ handle_switch_ready( uint64_t datapath_id, void *user_data ) {
     get_transaction_id(),
     get_cookie(),
     0,
-    0,
+    1, //table id = 1
     OFPFC_ADD,
     0,
     0,
@@ -268,39 +353,103 @@ handle_switch_ready( uint64_t datapath_id, void *user_data ) {
   );
   send_openflow_message( datapath_id, flow_mod );
   free_buffer( flow_mod );
-
   delete_instructions( insts );
-
   delete_actions( actions );
 }
 
 
 /**************************************
- ____  _____ ____ _____ _____ _   _ _          _    ____ ___
-|  _ \| ____/ ___|_   _|  ___| | | | |        / \  |  _ \_ _|
-| |_) |  _| \___ \ | | | |_  | | | | |       / _ \ | |_) | |
-|  _ <| |___ ___) || | |  _| | |_| | |___   / ___ \|  __/| |
-|_| \_\_____|____/ |_| |_|    \___/|_____| /_/   \_\_|  |___|
-
-*//////////////////////////////////////
+ *  ____  _____ ____ _____ _____ _   _ _          _    ____ ___
+ * |  _ \| ____/ ___|_   _|  ___| | | | |        / \  |  _ \_ _|
+ * | |_) |  _| \___ \ | | | |_  | | | | |       / _ \ | |_) | |
+ * |  _ <| |___ ___) || | |  _| | |_| | |___   / ___ \|  __/| |
+ * |_| \_\_____|____/ |_| |_|    \___/|_____| /_/   \_\_|  |___|
+ *
+ *********************************************/
 
 /*** Define your REST API callback function here ***/
 static char *
-handle_query_test_restapi( const struct mg_request_info *request_info, void *request_data, int len ) {
+handle_query_add_uds( const struct mg_request_info *request_info, void *request_data, int len ) {
+  char dpid[30];
+  memset(dpid,0,sizeof(dpid));
+  memcpy(dpid,&request_info->uri[9],strlen(request_info->uri)-8);
+  printf("dpid = %s\n",dpid);
   return "It is from test rest api...";
 }
 
 static char *
-handle_query_test2_restapi( const struct mg_request_info *request_info, void *request_data,int len ) {
-  json_object *new_obj;
-  printf("test2\n");
-  printf("data = %s\n",(char*)request_data);
-  new_obj = json_tokener_parse((char*)request_data);
-  printf("new_obj.to_string()=%s\n", json_object_to_json_string(new_obj));
-  json_object_put(new_obj); 
-
-  return "It is from test2 rest api...";
+handle_query_add_uds_all( const struct mg_request_info *request_info, void *request_data,int len ) {
+  int err;
+  const list_element *element;
+  for ( element = switches; element != NULL; element = element->next ) {
+      err = send_uds_flow( *(uint64_t*)element->data,request_data,len);
+	  switch(err){
+		case -1:
+			return "json format error\n";
+		case 0:
+			break;
+	  }
+  }
+  return "send uds flow\n";
 }
+
+
+int send_uds_flow(uint64_t datapath_id, char* request_data,int len){
+    
+  openflow_instructions *insts = create_instructions();
+  append_instructions_goto_table(insts,1);
+
+  oxm_matches *match = create_oxm_matches();
+  set_oxm_matches_from_json(match,request_data);
+  
+  buffer *flow_mod = create_flow_mod(
+    get_transaction_id(),
+    get_cookie(),
+    0,
+    0,  //table id
+    OFPFC_ADD,
+    0,
+    0,
+    OFP_HIGH_PRIORITY,
+    0, 
+    0,
+    0,
+    OFPFF_SEND_FLOW_REM,
+    match,
+    insts
+  );
+  send_openflow_message( datapath_id, flow_mod );
+  free_buffer( flow_mod );
+  delete_oxm_matches( match );
+  delete_instructions( insts );
+}
+
+int set_oxm_matches_from_json(oxm_matches* oxm_match,char* request_data){
+	int err = 0;
+	int i =0;
+	json_object *new_obj,*match;
+	json_object *src_mac;
+	char srcmac[6],src_mac_mask[6];
+    new_obj = json_tokener_parse(request_data);
+	if(!json_object_object_get_ex(new_obj,"match",&match)){
+		printf("parse match filed  error %s\n",json_object_get_string(new_obj));
+  		err = -1;
+		goto error;
+	}
+    if(json_object_object_get_ex(match,"eth_src",&src_mac)){
+		sscanf(json_object_get_string(src_mac), "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &srcmac[0], &srcmac[1], &srcmac[2], &srcmac[3], &srcmac[4], &srcmac[5]);
+		memset(src_mac_mask,255,sizeof(src_mac_mask));
+		for(i=0;i<6;i++)
+			printf("%d\n",srcmac[i]);
+		append_oxm_match_eth_src(oxm_match,srcmac,src_mac_mask);
+		json_object_put(src_mac);
+	}
+
+error:
+	json_object_put(new_obj);
+	return err;
+}
+
 /***************************************************/
 
 
@@ -310,6 +459,7 @@ main( int argc, char *argv[] ) {
   /* Initialize the Trema world */
   init_trema( &argc, &argv );
   
+  create_switches( &switches );
   /* Init restapi manager */
   init_restapi_manager();
   
@@ -317,15 +467,15 @@ main( int argc, char *argv[] ) {
   start_restapi_manager();
   
   /*** Add your REST API ***/
-  add_restapi_url( "^/test/abc$", "GET", handle_query_test_restapi );
-  add_restapi_url( "^/uds/add/all$", "PUT", handle_query_test2_restapi );
+  add_restapi_url( "^/uds/add/all$", "PUT", handle_query_add_uds_all );
+  add_restapi_url( "^/uds/add/", "PUT", handle_query_add_uds );
   /*************************/
   
   /* Set switch ready handler  (learning switch)*/
   hash_table *forwarding_db = create_hash( compare_forwarding_entry, hash_forwarding_entry );
   add_periodic_event_callback( AGING_INTERVAL, update_forwarding_db, forwarding_db );
   set_packet_in_handler( handle_packet_in, forwarding_db );
-  set_switch_ready_handler( handle_switch_ready, NULL );
+  set_switch_ready_handler( handle_switch_ready, &switches );
 
   /* Main loop */
   start_trema();
